fa3784c8f1c87ebe25961c3c7d0b0761512a89e7
Yves Van Broekhoven <yvesvanbroekhoven@gmail.com>
2012-12-17 16:14:32 +0100

    Initial commit + demo added

--- jquery-custom-checkbox.js
+++ jquery-custom-checkbox.js
@@ -2,7 +2,7 @@
  * jquery-custom-checkbox
  *
  * Created at: 2012-12-17 15:53:56 +0100
- * Updated at: 2012-12-17 15:53:56 +0100
+ * Updated at: 2012-12-17 15:54:42 +0100
  *
  * Author: Yves Van Broekhoven
  * Version: 0.0.0
@@ -13,4 +13,52 @@
 
 (function($) {
   "use strict";
+
+  var plugin_name = 'jqueryCustomCheckbox';
+
+  function CustomCheckbox( element ) {
+    var $this = $(element);
+
+    this.element  = element;
+    this.$label   = $this.siblings('label[for='+ $this.prop('name') +']');
+    this.$fake    = $('<span class="fake-checkbox"></span>');
+
+    this.init();
+  }
+
+  CustomCheckbox.prototype.init = function() {
+    var _this = this,
+        $this = $(_this.element);
+
+    $this.hide();
+
+    _this.$label
+      .addClass('custom-checkbox')
+      .append( _this.$fake );
+
+    _this.setState();
+
+    $this.on('change', function() {
+      _this.setState();
+    });
+  };
+
+  CustomCheckbox.prototype.setState = function() {
+    var $this = $(this.element);
+
+    if ( $this.prop('checked') === true ) {
+      this.$fake.addClass('checked');
+
+    } else {
+      this.$fake.removeClass('checked');
+
+    }
+  };
+
+  $.fn.customcheckbox = function() {
+    return this.each( function() {
+      $(this).data( plugin_name, new CustomCheckbox(this) );
+    });
+  };
+
 }(jQuery));
