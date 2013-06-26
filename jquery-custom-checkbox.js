a1b9eb5d9b170ec0b8ac8f191f39de7ce7c989f3
Yves Van Broekhoven <yvesvanbroekhoven@gmail.com>
2013-06-26 16:37:31 +0200

    Refactored so it works with groups & radio buttons

--- jquery-custom-checkbox.js
+++ jquery-custom-checkbox.js
@@ -2,7 +2,7 @@
  * jquery-custom-checkbox
  *
  * Created at: 2012-12-17 15:53:56 +0100
- * Updated at: 2012-12-17 16:17:57 +0100
+ * Updated at: 2013-06-26 16:34:38 +0200
  *
  * Author: Yves Van Broekhoven
  * Version: 1.0.0
@@ -14,17 +14,22 @@
 (function($) {
   "use strict";
 
+
   var plugin_name = 'jqueryCustomCheckbox';
+
 
   function CustomCheckbox( element ) {
     var $this = $(element);
 
-    this.element  = element;
-    this.$label   = $this.siblings('label[for='+ $this.prop('name') +']');
-    this.$fake    = $('<span class="fake-checkbox"></span>');
-
+    this.element      = element;
+    this.name         = $this.attr('name');
+    this.escaped_name = this.name.replace('[', '\\[').replace(']', '\\]');
+    this.$label       = $this.siblings('label[for='+ $this.attr('id') +']');
+    this.$fake        = $('<span class="fake-checkbox"></span>');
+    this.$groupling_labels = this.findGrouplingLabels();
     this.init();
   }
+
 
   CustomCheckbox.prototype.init = function() {
     var _this = this,
@@ -43,6 +48,7 @@
     });
   };
 
+
   CustomCheckbox.prototype.setState = function() {
     var $this = $(this.element);
 
@@ -53,7 +59,29 @@
       this.$fake.removeClass('checked');
 
     }
+
+    // If radio button, uncheck group members
+    if ( $this.attr('type') == 'radio' ) {
+      this.$groupling_labels.find('.fake-checkbox').removeClass('checked');
+    }
   };
+
+
+  CustomCheckbox.prototype.findGrouplingLabels = function() {
+    var $this    = $(this.element),
+        $options = $('input[name=' + this.escaped_name + ']').not($this),
+        $groupling_labels = $();
+
+    $options.each(function() {
+      var $option = $(this),
+          $label  = $option.siblings('label[for='+ $option.attr('id') +']');
+
+      $groupling_labels = $groupling_labels.add($label);
+    });
+
+    return $groupling_labels;
+  };
+
 
   $.fn.customcheckbox = function() {
     return this.each( function() {
--- public/jquery-custom-checkbox-min.js
+++ public/jquery-custom-checkbox-min.js
@@ -3,11 +3,12 @@
  jquery-custom-checkbox
 
  Created at: 2012-12-17 15:53:56 +0100
- Updated at: 2012-12-17 16:17:57 +0100
+ Updated at: 2013-06-26 16:34:38 +0200
 
  Author: Yves Van Broekhoven
  Version: 1.0.0
 
 */
-(function(b){function d(a){var c=b(a);this.element=a;this.$label=c.siblings("label[for="+c.prop("name")+"]");this.$fake=b('<span class="fake-checkbox"></span>');this.init()}d.prototype.init=function(){var a=this,c=b(a.element);c.hide();a.$label.addClass("custom-checkbox").append(a.$fake);a.setState();c.on("change",function(){a.setState()})};d.prototype.setState=function(){!0===b(this.element).prop("checked")?this.$fake.addClass("checked"):this.$fake.removeClass("checked")};b.fn.customcheckbox=function(){return this.each(function(){b(this).data("jqueryCustomCheckbox",
+(function(b){function d(a){var c=b(a);this.element=a;this.name=c.attr("name");this.escaped_name=this.name.replace("[","\\[").replace("]","\\]");this.$label=c.siblings("label[for="+c.attr("id")+"]");this.$fake=b('<span class="fake-checkbox"></span>');this.$groupling_labels=this.findGrouplingLabels();this.init()}d.prototype.init=function(){var a=this,c=b(a.element);c.hide();a.$label.addClass("custom-checkbox").append(a.$fake);a.setState();c.on("change",function(){a.setState()})};d.prototype.setState=
+function(){var a=b(this.element);!0===a.prop("checked")?this.$fake.addClass("checked"):this.$fake.removeClass("checked");"radio"==a.attr("type")&&this.$groupling_labels.find(".fake-checkbox").removeClass("checked")};d.prototype.findGrouplingLabels=function(){var a=b(this.element),a=b("input[name="+this.escaped_name+"]").not(a),c=b();a.each(function(){var a=b(this),a=a.siblings("label[for="+a.attr("id")+"]");c=c.add(a)});return c};b.fn.customcheckbox=function(){return this.each(function(){b(this).data("jqueryCustomCheckbox",
 new d(this))})}})(jQuery);
